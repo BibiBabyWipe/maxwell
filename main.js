6be7554d2a041041481ac42f5706d16a39dec2d8
BibiBabyWipe <148632892+BibiBabyWipe@users.noreply.github.com>
2024-05-03 02:32:09 +0700

    zz

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(4, 5, 20);
+camera.position.set(4, 5, 40);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
 controls.minDistance = 5;
-controls.maxDistance = 80;
+controls.maxDistance = 160;
 controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = false;
@@ -41,7 +41,7 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 30, 100, 0.22, 1);
+const spotLight = new THREE.SpotLight(0xffffff, 300, 100, 0.22, 1);
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
