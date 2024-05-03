4e04a854207813a71e22e9af5edee38605982dd8
BibiBabyWipe <148632892+BibiBabyWipe@users.noreply.github.com>
2024-05-03 21:06:40 +0700

    zz

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.outputColorSpace = THREE.sRGBEncoding;
-renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xf0f0f0);
-renderer.physicallyCorrectLights = true;
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
@@ -32,15 +30,6 @@ controls.autoRotate = false;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
-const ambientLight = new THREE.AmbientLight(0xf0f0f0, 0.8);
-scene.add(ambientLight);
-
-const spotLight = new THREE.SpotLight(0xf0f0f0, 300, 1000, 0.22, 1);
-spotLight.position.set(0, 100, 0);
-spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
-scene.add(spotLight);
-
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
 groundGeometry.rotateX(-Math.PI / 2);
 const groundMaterial = new THREE.MeshStandardMaterial({
@@ -52,15 +41,12 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-<<<<<<< HEAD
-=======
 const spotLight = new THREE.SpotLight(0xffffff, 300, 1000, 0.22, 1);
-spotLight.position.set(0, 25, 0);
+spotLight.position.set(0, 100, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
->>>>>>> parent of 2c43633 (Update main.js)
 const loader = new GLTFLoader().setPath('public/maxwell/');
 loader.load('scene.gltf', (gltf) => {
   console.log('loading model');
@@ -68,7 +54,6 @@ loader.load('scene.gltf', (gltf) => {
 
   mesh.traverse((child) => {
     if (child.isMesh) {
-      child.material = new THREE.MeshStandardMaterial({ color: 0xffffff });
       child.castShadow = true;
       child.receiveShadow = true;
     }
